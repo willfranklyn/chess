5dd79cf42491fd9b298ea606625901dc4ec74dfd
willfranklyn <willfranklyn2@gmail.com>
2023-01-07 22:53:49 -0700

    First Commit

---
+++ components/Home.vue
@@ -0,0 +1,110 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css" rel="stylesheet">
+    <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+      <div class="text-center"><h1 class="h1">Chess</h1></div>
+      <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6">
+        <div class="d-flex flex-row">
+          <span class="flex-fill m-auto pr-3">8</span>
+          <div class="p-8 bg-dark"></div>
+          <div class="p-8 bg-secondary"></div>
+          <div class="p-8 bg-dark"></div>
+          <div class="p-8 bg-secondary"></div>
+          <div class="p-8 bg-dark"></div>
+          <div class="p-8 bg-secondary"></div>
+          <div class="p-8 bg-dark"></div>
+          <div class="p-8 bg-secondary"></div>
+        </div>
+        <div class="d-flex">
+          <div class="flex-fill m-auto pr-3">7</div>
+          <div class="p-8 bg-secondary"></div>
+          <div class="p-8 bg-dark"></div>
+          <div class="p-8 bg-secondary"></div>
+          <div class="p-8 bg-dark"></div>
+          <div class="p-8 bg-secondary"></div>
+          <div class="p-8 bg-dark"></div>
+          <div class="p-8 bg-secondary"></div>
+          <div class="p-8 bg-dark"></div>
+        </div>
+        <div class="d-flex">
+          <div class="flex-fill m-auto pr-3">6</div>
+          <div class="p-8 bg-dark"></div>
+          <div class="p-8 bg-secondary"></div>
+          <div class="p-8 bg-dark"></div>
+          <div class="p-8 bg-secondary"></div>
+          <div class="p-8 bg-dark"></div>
+          <div class="p-8 bg-secondary"></div>
+          <div class="p-8 bg-dark"></div>
+          <div class="p-8 bg-secondary"></div>
+        </div>
+        <div class="d-flex">
+          <div class="flex-fill m-auto pr-3">5</div>
+          <div class="p-8 bg-secondary"></div>
+          <div class="p-8 bg-dark"></div>
+          <div class="p-8 bg-secondary"></div>
+          <div class="p-8 bg-dark"></div>
+          <div class="p-8 bg-secondary"></div>
+          <div class="p-8 bg-dark"></div>
+          <div class="p-8 bg-secondary"></div>
+          <div class="p-8 bg-dark"></div>
+        </div>
+        <div class="d-flex">
+          <div class="flex-fill m-auto pr-3">4</div>
+          <div class="p-8 bg-dark"></div>
+          <div class="p-8 bg-secondary"></div>
+          <div class="p-8 bg-dark"></div>
+          <div class="p-8 bg-secondary"></div>
+          <div class="p-8 bg-dark"></div>
+          <div class="p-8 bg-secondary"></div>
+          <div class="p-8 bg-dark"></div>
+          <div class="p-8 bg-secondary"></div>
+        </div>
+        <div class="d-flex">
+          <div class="flex-fill m-auto pr-3">3</div>
+          <div class="p-8 bg-secondary"></div>
+          <div class="p-8 bg-dark"></div>
+          <div class="p-8 bg-secondary"></div>
+          <div class="p-8 bg-dark"></div>
+          <div class="p-8 bg-secondary"></div>
+          <div class="p-8 bg-dark"></div>
+          <div class="p-8 bg-secondary"></div>
+          <div class="p-8 bg-dark"></div>
+        </div>
+        <div class="d-flex">
+          <div class="flex-fill m-auto pr-3">2</div>
+          <div class="p-8 bg-dark"></div>
+          <div class="p-8 bg-secondary"></div>
+          <div class="p-8 bg-dark"></div>
+          <div class="p-8 bg-secondary"></div>
+          <div class="p-8 bg-dark"></div>
+          <div class="p-8 bg-secondary"></div>
+          <div class="p-8 bg-dark"></div>
+          <div class="p-8 bg-secondary"></div>
+        </div>
+        <div class="d-flex">
+          <div class="flex-fill m-auto pr-3">1</div>
+          <div class="p-8 bg-secondary"></div>
+          <div class="p-8 bg-dark"></div>
+          <div class="p-8 bg-secondary"></div>
+          <div class="p-8 bg-dark"></div>
+          <div class="p-8 bg-secondary"></div>
+          <div class="p-8 bg-dark"></div>
+          <div class="p-8 bg-secondary"></div>
+          <div class="p-8 bg-dark"></div>
+        </div>
+        <div class="d-flex">
+          <div class="flex-fill m-auto pr-3"></div>
+          <div class="px-7 pt-1">a</div>
+          <div class="px-7 pt-1">b</div>
+          <div class="px-7 pt-1">c</div>
+          <div class="px-7 pt-1">d</div>
+          <div class="px-7 pt-1">e</div>
+          <div class="px-7 pt-1">f</div>
+          <div class="px-7 pt-1">g</div>
+          <div class="px-7 pt-1">h</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
